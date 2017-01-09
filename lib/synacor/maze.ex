d827897d536122c570bccfaa9436f445ad19087c
John Warwick <jwarwick@gmail.com>
2017-01-08 20:54:30 -0500

    Reading all rooms

--- lib/synacor/maze.ex
+++ lib/synacor/maze.ex
@@ -12,15 +12,36 @@ defmodule Synacor.Maze do
   # Strings are pascal-style, the first instruction tells how many letters follow
   # Lists have a leading instruction telling the length of the list
   #
+  # Address 2732 holds the address of the current room
   # Starting room is at address 2317 (read from address 2732)
   # First instruction is a pointer to the room title
   # Second instruction is a pointer to the room description
   # Third instruction is a pointer to a list of possible directions
   #  The list is pointers to direction strings
-  # Fourth instruction is a pointer to another block of data: TBD
+  # Fourth instruction is a pointer to a list of the rooms for the given directions
+  # Fifth instruction is items?
+  # There appear to be blocks that don't quite match this pattern, maybe still encrypted?
 
   defmodule Room do
-    defstruct offset: nil, title: nil, description: nil, directions: []
+    defstruct offset: nil, title: nil, description: nil, neighbors: []
+  end
+
+  @doc """
+  Read all rooms
+  """
+  def read_all_rooms(start_room_offset, bin) do
+    read_next([start_room_offset], Map.new(), bin)
+  end
+
+  defp read_next([], map, _bin), do: map
+  defp read_next(l = [room | rest], map, bin) do
+    r = read_room(room, bin)
+    neighbors =
+      r.neighbors
+      |> Enum.map(fn {_str, num} -> num end)
+      |> Enum.reject(&(Map.has_key?(map, &1)))
+      |> Enum.reject(&(Enum.member?(l, &1)))
+    read_next(neighbors ++ rest, Map.put(map, r.offset, r), bin)
   end
   
   @doc """
@@ -30,8 +51,10 @@ defmodule Synacor.Maze do
     title = get_string_ptr(offset, bin)
     desc = get_string_ptr(offset+1, bin)
     dir_list = get_string_ptr(offset+2, bin)
-    dirs = Enum.map(dir_list, &(read_string(&1, bin)))
-    %Room{offset: offset, title: title, description: desc, directions: dirs}
+    names = Enum.map(dir_list, &(read_string(&1, bin)))
+    rooms = get_string_ptr(offset+3, bin)
+    z = Enum.zip(names, rooms)
+    %Room{offset: offset, title: title, description: desc, neighbors: z}
   end
 
   defp get_string_ptr(offset, bin) do
